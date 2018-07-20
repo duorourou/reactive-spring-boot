c1f880d1732f59361529ebc5298441685c0cf4c9
duorourou <duoorourou@gmail.com>
2018-07-20 14:06:52 +0800

    [#000] [Duorourou] add reactiveController insteadof Router

--- src/main/java/com/duorourou/reactive/practices/repo/UserRepository.java
+++ src/main/java/com/duorourou/reactive/practices/repo/UserRepository.java
@@ -2,6 +2,8 @@ package com.duorourou.reactive.practices.repo;
 
 import com.duorourou.reactive.practices.domain.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.repository.reactive.RxJava2CrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
--- src/main/java/com/duorourou/reactive/practices/router/UserRouter.java
+++ src/main/java/com/duorourou/reactive/practices/router/UserRouter.java
@@ -1,30 +1,23 @@
 package com.duorourou.reactive.practices.router;
 
-import com.duorourou.reactive.practices.domain.User;
 import com.duorourou.reactive.practices.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.path;
-import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Component
 public class UserRouter {
     @Autowired
     private UserRepository userRepository;
 
-    @Bean
-    public RouterFunction<ServerResponse> handle() {
-
-        return nest(path("/users"),
-                route(RequestPredicates.GET("/{name}"),
-                        request -> ok().body(userRepository.findByName(request.pathVariable("name")), User.class))
-        );
-    }
+//    @Bean
+//    public RouterFunction<ServerResponse> handle() {
+//
+//        return nest(path("/users"),
+//                route(RequestPredicates.GET("/{name}"),
+//                        request -> ok().body(userRepository.findByName(request.pathVariable("name")), User.class))
+//                        .andRoute(RequestPredicates.POST("/"),
+//                                request -> status(HttpStatus.CREATED)
+//                                        .body(fromPublisher(userRepository.insert(request.bodyToMono(User.class)), User.class)))
+//        );
+//    }
 }
